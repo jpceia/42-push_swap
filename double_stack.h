ce578271511d0b07849395a2c894a38500e520cd
jpceia <joao.p.ceia@gmail.com>
2021-08-12 12:33:14 +0100

    fixed norm errors

--- double_stack.h
+++ double_stack.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   double_stack.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpceia <jpceia@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/07 23:50:24 by jpceia            #+#    #+#             */
-/*   Updated: 2021/08/09 19:31:43 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/08/12 11:51:04 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,28 @@
 # include "libft.h"
 # include "stack.h"
 
-# define MIN(a,b) (((a)<(b))?(a):(b))
-# define MAX(a,b) (((a)>(b))?(a):(b))
-
-typedef struct	s_double_stack
+typedef struct s_double_stack
 {
 	t_stack	*a;
 	t_stack	*b;
 }				t_double_stack;
 
-void	double_stack_print(t_double_stack ss);
+void			double_stack_print(t_double_stack ss);
 t_double_stack	*double_stack_init(t_double_stack *ss_ref);
-void	double_stack_clear(t_double_stack *ss_ref);
+void			double_stack_clear(t_double_stack *ss_ref);
 
-void	operation_pa(t_double_stack *ss_ref);
-void	operation_pb(t_double_stack *ss_ref);
-void	operation_sa(t_double_stack *ss_ref);
-void	operation_sb(t_double_stack *ss_ref);
-void	operation_ss(t_double_stack *ss_ref);
+void			operation_pa(t_double_stack *ss_ref);
+void			operation_pb(t_double_stack *ss_ref);
+void			operation_sa(t_double_stack *ss_ref);
+void			operation_sb(t_double_stack *ss_ref);
+void			operation_ss(t_double_stack *ss_ref);
 
-void	operation_ra(t_double_stack *ss_ref);
-void	operation_rb(t_double_stack *ss_ref);
-void	operation_rr(t_double_stack *ss_ref);
+void			operation_ra(t_double_stack *ss_ref);
+void			operation_rb(t_double_stack *ss_ref);
+void			operation_rr(t_double_stack *ss_ref);
 
-void	operation_rra(t_double_stack *ss_ref);
-void	operation_rrb(t_double_stack *ss_ref);
-void	operation_rrr(t_double_stack *ss_ref);
+void			operation_rra(t_double_stack *ss_ref);
+void			operation_rrb(t_double_stack *ss_ref);
+void			operation_rrr(t_double_stack *ss_ref);
 
 #endif
