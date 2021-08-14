7a4b1193036dfe5179bf46041ff6afc9c9168add
jpceia <joao.p.ceia@gmail.com>
2021-08-14 14:10:30 +0100

    added ft_remainder

--- libft/libft_bonus.h
+++ libft/libft_bonus.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpceia <jpceia@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 06:05:31 by jpceia            #+#    #+#             */
-/*   Updated: 2021/08/13 09:37:24 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/08/14 14:09:13 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 # include "libft.h"
 
-int     ft_strcmp(char *s1, char *s2);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_remainder(int a, int b);
 char	*ft_lltoa(long long nb);
 char	*ft_ulltoa(unsigned long long nb);
 char	*ft_lltoa_base(long long nb, const char *base);
