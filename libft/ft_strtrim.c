16b172f1af80e888c7bd9617c159a78267580f34
Joao Ceia <joao.p.ceia@gmail.com>
2021-08-20 22:44:11 +0100

    removed ft_contains

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -3,22 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jpceia <jpceia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 18:50:03 by jpceia            #+#    #+#             */
-/*   Updated: 2021/04/09 17:50:08 by jceia            ###   ########.fr       */
+/*   Updated: 2021/08/19 21:52:29 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_contains(char c, char const *charset)
-{
-	while (*charset)
-		if (c == *charset++)
-			return (1);
-	return (0);
-}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
